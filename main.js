7b8236262ca6efed3878089ad40a0170fe360b7c
umair786582 <160109939+umair786582@users.noreply.github.com>
2024-04-17 00:32:07 +0500

    simple words counter

---
+++ main.js
@@ -0,0 +1,17 @@
+#! /user/bin/env node
+// this line is called shebang 
+// impoet the module which is a command line interface for node.js
+import inquirer from "inquirer";
+// declare a constant answers and assign it to the result og inquirer.promt function
+const answers = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "enter your sentence to count the word: "
+    }
+]);
+const words = answers.sentence.trim().split(" ");
+// print the array of words to the console
+console.log(words);
+// print the word count of the sentence to the console
+console.log(`your sentence word count is  ${words.length}`);
